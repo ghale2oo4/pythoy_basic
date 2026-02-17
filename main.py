03d72e01b1439eebcf3cfaefa220312b921ac5bf
ghalepaat <ghalepaat@gmail.com>
2026-02-17 13:21:32 +0545

    changes main.py to display greeting

--- main.py
+++ main.py
@@ -1,6 +1,2 @@
-num1 = 2
-num2 = 3
-print("result: ", num1 +num2);
-
-print("now this is the version 2");
-print("now updating to version 3");
+name = input("enter you name: ")
+print(f"hello {name}, welcome to devops journey.")
